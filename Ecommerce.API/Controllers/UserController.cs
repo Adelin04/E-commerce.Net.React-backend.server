5f7f42c9003345164be0f92bbe6e48e74338e85b
Adelin04 <adelin.marin04@yahoo.com>
2022-11-23 07:16:05 +0200

    created UserController.cs

--- Ecommerce.API/Controllers/UserController.cs
+++ Ecommerce.API/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Ecommerce.API.Services;
+﻿using Ecommerce.API.Contracts;
+using Ecommerce.API.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.API.Controllers;
@@ -19,6 +21,62 @@ public class UserController : ControllerBase
     [HttpGet("getAllUsers")]
     public async Task<ActionResult> GetAllUsers()
     {
-        return null;
+        try
+        {
+            var listAllUsers = await this._userService.GetAllUsers();
+
+            if (listAllUsers is not null)
+                return Ok(new { Success = true, CounterUser = listAllUsers.Count });
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Error -> " + exception.Message);
+            this.Logger.LogInformation("Error -> " + exception.Message);
+            return BadRequest(new { Error = exception.Message });
+        }
+
+        return BadRequest(new { Success = false, Message = "No users!" });
+    }
+
+    [HttpGet("get/userById/{id}")]
+    public async Task<ActionResult> GetUserById([FromHeader] long id)
+    {
+        try
+        {
+            var userById = await this._userService.GetUserById(id);
+
+            if (userById is not null)
+                return Ok(new { Success = true, userById = userById });
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Error -> " + exception.Message);
+            this.Logger.LogInformation("Error -> " + exception.Message);
+        }
+
+        return BadRequest(new { Success = false, Message = "No user!" });
+    }
+
+    [HttpGet("update/userById/{id}")]
+    public async Task<ActionResult> UpdateUserById([FromHeader] long id, [FromBody] UserDataUpdate userDataUpdate)
+    {
+        try
+        {
+            var userById = await this._userService.GetUserById(id);
+
+            if (userById is not null)
+            {
+                var userUppdated = await this._userService.UpdateUserById(id, userDataUpdate);
+                return Ok(new { Success = true, UserUpdated = userUppdated });
+            }
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine("Error -> " + exception.Message);
+            this.Logger.LogInformation("Error -> " + exception.Message);
+            return BadRequest(new { Error = exception.Message });
+        }
+
+        return BadRequest(new { Success = false, Message = "No user!" });
     }
 }
