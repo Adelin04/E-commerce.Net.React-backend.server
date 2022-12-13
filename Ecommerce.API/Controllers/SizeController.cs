20ca99a1aa0af473b8f77270b9339c6accd11f71
Adelin04 <adelin.marin04@yahoo.com>
2022-12-13 14:54:09 +0200

    updated server ecommerce V1.0

---
+++ Ecommerce.API/Controllers/SizeController.cs
@@ -0,0 +1,39 @@
+﻿using Ecommerce.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]/v1/")]
+public class SizeController : ControllerBase
+{
+    private readonly SizeService _sizeService;
+    private readonly ILogger<ControllerBase> Logger;
+
+    public SizeController(SizeService sizeService,ILogger<ControllerBase> logger)
+    {
+        this._sizeService = sizeService;
+        this.Logger = logger;
+    }
+
+    [HttpGet("get/allSizes")]
+    public async Task<ActionResult> GetAllSizes()
+    {
+        try
+        {
+            var allSizes = await this._sizeService.GetAllSize_ServiceAsync();
+
+            if (allSizes is not null)
+            {
+                Logger.LogInformation($"Returned all sizes {allSizes}");
+                return Ok(new {Success = true, Sizes = allSizes, NrSizes = allSizes.Count});
+            }
+        }
+        catch (Exception exception)
+        {
+                Logger.LogInformation($"Error -> {exception.Message}");
+        }
+                Logger.LogInformation($"The sizes entity could not be found!");
+                return NotFound(new {Success = false});
+    }
+}
